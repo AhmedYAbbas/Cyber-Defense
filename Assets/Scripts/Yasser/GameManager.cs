6deac400a8fa42ea34c1256ea9b5204b305a6592
Amjad Mohamed <amjadmohamed55@gmail.com>
2023-05-09 19:05:25 +0300

    Small Changes

--- Assets/Scripts/Yasser/GameManager.cs
+++ Assets/Scripts/Yasser/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviourPunCallbacks
 {
     #region Public Variables
 
-    public GameManager Instance { get; private set; }
+    public static GameManager Instance { get; private set; }
     public GameObject TowerManager;
     #endregion
 
@@ -29,17 +29,6 @@ public class GameManager : MonoBehaviourPunCallbacks
         UILayer.Instance.timerGameObject.SetActive(true);
         UILayer.Instance.LoadPlayerUI((MatchManager.Side)PhotonNetwork.LocalPlayer.CustomProperties[CustomKeys.P_SIDE]);
         //print(((MatchManager.Side)PhotonNetwork.LocalPlayer.CustomProperties[CustomKeys.P_SIDE]).ToString());
-        if((MatchManager.Side)PhotonNetwork.LocalPlayer.CustomProperties[CustomKeys.P_SIDE] == MatchManager.Side.Defender)
-        {
-            TowerManager.gameObject.SetActive(true);
-            //print("I AM A DEFENDER");
-        }
-        else
-        {
-            //print("I AM AN ATTACKER");
-            TowerManager.gameObject.SetActive(false);
-        }
-
     }
     #endregion
 }
