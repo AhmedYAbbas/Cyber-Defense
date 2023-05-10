5b9b75f42cfa037ee361f026ac3dfbdfc64b1413
Amjad Mohamed <amjadmohamed55@gmail.com>
2023-05-10 23:22:05 +0300

    Small Bug Fix In Enemy Pool

--- Assets/Scripts/Amjad/MoveToTarget.cs
+++ Assets/Scripts/Amjad/MoveToTarget.cs
@@ -5,10 +5,15 @@ public class MoveToTarget : MonoBehaviour
 {
     private NavMeshAgent _agent;
     private GameObject _target;
-    void Start()
+
+    void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
         _target = GameObject.FindGameObjectWithTag("Tower");
+    }
+
+    private void OnEnable()
+    {
         if (_target != null)
         {
             print("Target Found");
