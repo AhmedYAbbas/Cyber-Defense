c4b57a26f206546c09ffaa8aeb635167a70f7d08
SamySorial99 <76940735+SamySorial99@users.noreply.github.com>
2023-05-15 14:34:55 +0300

    Revert "Revert "Merge branch 'Development' into Feature-TowerTouchInput""
    This reverts commit d5c37c482c8dc1653acbb0db2acd17ddd98b012f.

---
+++ Assets/Scripts/Eissa/Object Pooling/PoolableObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+
+public class PoolableObject : MonoBehaviour
+{
+    public ObjectPool Parent;
+
+    public virtual void OnDisable()
+    {
+        Parent.ReturnObjectToPool(this);
+    }
+}
