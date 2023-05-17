d95f50f355fa6b67ec81014823bf02a6054a2b36
Dnubis-eissa <mohamedesaa38@gmail.com>
2023-05-17 14:54:21 +0300

    tower health bar working still working on the malware health bar bug

---
+++ Assets/Scripts/Eissa/UILookAtCamera.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Eissa
+{
+    public class UILookAtCamera : MonoBehaviour
+    {
+        private Camera _camera;
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        private void LateUpdate()
+        {
+            transform.LookAt(transform.position + _camera.transform.rotation*Vector3.forward,_camera.transform.rotation*Vector3.up);
+        }
+    }
+}
